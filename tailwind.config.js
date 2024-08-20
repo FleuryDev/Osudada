9180e715126e0f48b0182e913fbcf72d641b7fac
Fleury Setondji. <120659283+FleuryDev@users.noreply.github.com>
2024-08-20 15:49:09 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   theme: {
     extend: {},
   },
+  theme: {
+    fontSize: {
+      xs: ['12px','16px'],
+      sm: ['14px', '20px'],
+      base: ['15px', '22px'],
+      lg: ['16px', '24px'],
+      xl: ['18px', '28px'],
+    }
+  },
   plugins: [
     require('flowbite/plugin')
   ],
