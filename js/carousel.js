b6cea5197028d6a56a67566a310f7571530a90bf
Fleury Setondji. <120659283+FleuryDev@users.noreply.github.com>
2024-08-22 22:11:09 +0100

    UPDATE

---
+++ js/carousel.js
@@ -0,0 +1,1 @@
+const vaj=2
--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-import { Carousel } from 'flowbite';
-const carouselElement = document.getElementById('carousel-example');
+const carouselElement = document.getElementById('default-carouselA');
 
-
+const items = [
+    {
+        position: 0,
+        el: document.getElementById('carousel-item-1'),
+    },
+    {
+        position: 1,
+        el: document.getElementById('carousel-item-2'),
+    },
+   
+];
 
 // options with default values
 const options = {
@@ -9,18 +18,41 @@ const options = {
     interval: 3000,
 
     indicators: {
-        activeClasses: 'bg-orange-700 dark:bg-gray-800',
-        
+        activeClasses: 'bg-white dark:bg-gray-800',
+        inactiveClasses:
+            'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+        items: [
+            {
+                position: 0,
+                el: document.getElementById('carousel-indicator-1'),
+            },
+            {
+                position: 1,
+                el: document.getElementById('carousel-indicator-2'),
+            },
+           
+        ],
     },
-
-
+ 
+   
 };
 
 // instance options object
 const instanceOptions = {
-  id: 'default-carousel',
+  id: 'default-carouselA',
   override: true
 };
 
 
-const carousel = new Carousel(carouselElement, null, options, instanceOptions);
+const carousel = new Carousel(carouselElement, items, options, instanceOptions);
+
+// goes to the next (right) slide
+carousel.next();
+
+// goes to the previous (left) slide
+carousel.prev();
+
+// jumps to the 3rd position (position starts from 0)
+carousel.slideTo(0);
+
+carousel.cycle();
